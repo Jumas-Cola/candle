b6f52a7a5a295d9009d2f24b676b26bd7b9550d6
Jumas Cola <kbbyfl91@gmail.com>
2022-04-12 23:17:24 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Candle from "./components/Candle.vue";
+</script>
+
+<template>
+  <main>
+    <Candle v-for="c in candles" />
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      candles: 15892,
+    };
+  },
+};
+</script>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  margin: 0 auto;
+  background: #000000;
+}
+</style>
---
+++ src/components/Candle.vue
@@ -0,0 +1,10 @@
+<template>
+  <img src="../assets/candle.gif" alt="candle" loading="lazy" class="candle" />
+</template>
+
+<style scoped>
+.candle {
+  width: 9%;
+  margin: auto;
+}
+</style>
